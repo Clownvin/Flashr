91497c55f8c41d0e722ae11b5ae797f7ce72389d
Clownvin <123clownvin@gmail.com>
2024-07-13 15:50:57 -0500

    test: Add test for random traits

--- src/random.rs
+++ src/random.rs
@@ -47,3 +47,87 @@ impl<T> GetRandom for [T] {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::random::IterShuffled;
+
+    use super::GetRandom;
+
+    trait MinMax<T> {
+        fn min_max(&self) -> (&T, &T);
+    }
+
+    impl<T: PartialOrd> MinMax<T> for (T, T) {
+        fn min_max(&self) -> (&T, &T) {
+            let (l, r) = self;
+            if l < r {
+                (l, r)
+            } else {
+                (r, l)
+            }
+        }
+    }
+
+    #[test]
+    fn test_get_random() {
+        let rng = &mut rand::thread_rng();
+
+        let mut vals = vec![];
+        assert!(vals.get_random(rng).is_none());
+
+        vals.push(1);
+        for _ in 0..10 {
+            assert!(matches!(vals.get_random(rng), Some(1)));
+        }
+
+        vals.push(2);
+        let mut seen = (0, 0);
+        const TOTAL: usize = 1000;
+        for _ in 0..TOTAL {
+            let val = vals.get_random(rng).unwrap();
+            if *val == 1 {
+                seen.0 += 1;
+            } else {
+                seen.1 += 1;
+            }
+        }
+
+        let (min, _) = seen.min_max();
+        assert!(
+            *min as f64 / TOTAL as f64 > 0.45,
+            "{min} is not around half of {TOTAL}"
+        );
+    }
+
+    #[test]
+    fn test_iter_shuffled() {
+        let rng = &mut rand::thread_rng();
+
+        let mut vals = vec![];
+        assert!(vals.clone().iter_shuffled(rng).next().is_none());
+
+        vals.push(1);
+        for _ in 0..10 {
+            let mut iter = vals.clone().iter_shuffled(rng);
+            assert!(matches!(iter.next(), Some(1)));
+            assert!(iter.next().is_none());
+        }
+
+        vals.push(2);
+        let mut seen = (0, 0);
+        const TOTAL: usize = 1000;
+        for _ in 0..TOTAL {
+            vals.clone()
+                .iter_shuffled(rng)
+                .enumerate()
+                .for_each(|(i, v)| if v == 1 { seen.0 += i } else { seen.1 += i })
+        }
+
+        let (min, _) = seen.min_max();
+        assert!(
+            *min as f64 / TOTAL as f64 > 0.45,
+            "{min} is not around half of {TOTAL}"
+        );
+    }
+}
