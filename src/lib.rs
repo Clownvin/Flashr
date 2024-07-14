a5eaee979515f36b3f6694242544d09b786bd5b0
Clownvin <123chall@gmail.com>
2024-07-14 02:50:33 -0500

    feat: Expand program help

--- src/lib.rs
+++ src/lib.rs
@@ -19,19 +19,29 @@ type ProblemCount = Option<usize>;
 #[derive(Parser, Debug)]
 #[command(name = "flashr")]
 struct FlashrCli {
-    #[arg(short = 'c', long = "count", value_name = "PROBLEM_COUNT")]
+    #[arg(short = 'c', long = "count", value_name = "PROBLEM_COUNT", help = "Number of problems to show.", long_help = COUNT_HELP)]
     problem_count: Option<usize>,
     #[arg(
         short = 'f',
         long = "faces",
         value_name = "[...FACE_N]",
-        long_help = "Faces to show questions for. Example Usage: flashr -f Front -f Back ./decks"
+        help = "Faces to show problems for.",
+        long_help = FACES_HELP
     )]
     faces: Option<Vec<String>>,
-    #[arg(short = 'm', long = "mode", default_value_t = Mode::Match, value_name = "MODE")]
+    #[arg(short = 'm', long = "mode", default_value_t = Mode::Match, value_name = "MODE", help = "Program mode", long_help = MODE_HELP)]
     mode: Mode,
+    #[arg(last = true, help = "Deck JSON file/dir paths", long_help = PATHS_HELP)]
     paths: Vec<String>,
 }
+
+const COUNT_HELP: &str = r#"Number of problems to show. If omitted, will continue indefinitely."#;
+const FACES_HELP: &str = r#"Faces to show problems for.
+Example Usage: flashr -f Front -f Back ./decks"#;
+const MODE_HELP: &str = r#"Possible values:
+    match   - Multiple choice matching problems
+    type    - Shown a face, and asked to type the answer"#;
+const PATHS_HELP: &str = r#"Paths to load decks from. Can be individual files or directories."#;
 
 #[derive(Clone, Debug)]
 enum Mode {
