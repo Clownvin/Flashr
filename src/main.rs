1cf08a12871c887ec3bfcb11aacdc580d60a9edd
Clownvin <123chall@gmail.com>
2024-07-28 21:41:14 -0500

    feat: Expand exit messages

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,35 @@ fn main() {
     let result = flashr::run();
     match result {
         Ok((total_correct, total)) => {
+            let percent_correct = if total == 0 {
+                0.0
+            } else {
+                (total_correct as f64 / total as f64) * 100.0
+            };
+
             println!(
                 "You got {total_correct} correct out of {total} ({:.2}%)",
-                if total == 0 {
-                    0.0
-                } else {
-                    (total_correct as f64 / total as f64) * 100.0
-                }
+                if total == 0 { 0.0 } else { percent_correct }
             );
-            if total_correct == total && total > 0 {
-                println!("Well done!");
+
+            if total >= 10 {
+                if percent_correct == 100.0 {
+                    if total >= 1000 {
+                        println!("🌌🌟🚀 Out of this world! 🚀🌟🌌")
+                    } else if total >= 100 {
+                        println!("🚀🌌 Spectacular! 🌌🚀")
+                    } else {
+                        println!("🌟 Perfect! 🌟");
+                    }
+                } else if percent_correct >= 0.9 {
+                    println!("🥇 Excellent! 🥇");
+                } else if percent_correct >= 0.8 {
+                    println!("🥈 Well done! 🥈");
+                } else if percent_correct >= 0.7 {
+                    println!("🥉 Nice! 🥉");
+                } else {
+                    println!("Keep up the practice!");
+                }
             }
         }
         Err(err) => {
